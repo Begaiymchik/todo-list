3dc182de8264c5eb76cfb76b122ff7c63feeb9fe
Begaiymchik <begaiym.tutueva@gmail.com>
2023-01-12 19:28:59 +0600

    add app

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css'
+import './img/mainBgTest.jpg'
+
+function App () {
+    return (
+        <div className='main'>
+           <div className="todo-container">
+
+           </div>
+        </div>
+    );
+}
+
+export default App;
